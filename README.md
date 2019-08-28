24c4b07385844a2b15ca3b6e1044087aead9eae6
Geoff Bourne <itzgeoff@gmail.com>
2019-08-27 22:14:00 -0500

    Add support for matching by attribute

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 
-A little utility (written in Go) that
-enables REST-like access to HTML pages by scraping and parsing them into JSON.
+A little utility (written in Go) that enables REST-like access to HTML pages by scraping and parsing them into JSON.
 
 ```
 usage: restify [<flags>] <url>
 
 Flags:
-  --help         Show context-sensitive help (also try --help-long and
-                 --help-man).
-  --class=CLASS  If specified, first-level elements encountered with this class
-                 will be extracted.
+  --help                 Show context-sensitive help (also try --help-long and
+                         --help-man).
+  --class=CLASS          If specified, first-level elements encountered with
+                         this class will be extracted.
+  --id=ID                If specified, the element with this id will be
+                         extracted.
+  --attribute=ATTRIBUTE  If specified, as key=value, the element with the given
+                         attribute name set to the given value is extracted.
+  --version              Print version and exit
 
 Args:
   <url>  A URL to RESTify into JSON
@@ -34,4 +38,26 @@ structured as:
     ...jsonNodes, if present...
   ]
 }
+```
+
+## Examples
+
+Locate the latest Minecraft Bedrock server version by picking off the `<a>`'s with `data-platform` set:
+
+```bash
+restify --attribute=data-platform https://www.minecraft.net/en-us/download/server/bedrock/
+```
+
+which produces:
+```json
+[
+  {"name":"a","attributes":{"data-platform":"serverBedrockWindows","role":"button"},"class":"btn btn-disabled-outline mt-4 downloadlink","href":"https://minecraft.azureedge.net/bin-win/bedrock-server-1.12.0.28.zip","text":"Download"},
+  {"name":"a","attributes":{"data-platform":"serverBedrockLinux","role":"button"},"class":"btn btn-disabled-outline mt-4 downloadlink","href":"https://minecraft.azureedge.net/bin-linux/bedrock-server-1.12.0.28.zip","text":"Download"}
+]
+```
+
+or to grab just the Linux instance:
+
+```bash
+restify --attribute=data-platform=serverBedrockLinux https://www.minecraft.net/en-us/download/server/bedrock/
 ```
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
@@ -13,14 +14,21 @@ import (
 
 var (
 	version = "dev"
-	commit = "master"
+	commit  = "master"
 )
 
 var (
-	url = kingpin.Arg("url", "A URL to RESTify into JSON").Required().URL()
-	byClass = kingpin.Flag("class", "If specified, first-level elements encountered with this class will be extracted.").String()
-	byId = kingpin.Flag("id", "If specified, the element with this id will be extracted.").String()
-	showVersion = kingpin.Flag("version", "Print version and exit").Bool()
+	url = kingpin.Arg("url", "A URL to RESTify into JSON").
+		Required().URL()
+	byClass = kingpin.Flag("class", "If specified, first-level elements encountered with this class will be extracted.").
+		String()
+	byId = kingpin.Flag("id", "If specified, the element with this id will be extracted.").
+		String()
+	byAttribute = kingpin.Flag("attribute",
+		"If specified, as key=value, the element with the given attribute name set to the given value is extracted.").
+		String()
+	showVersion = kingpin.Flag("version", "Print version and exit").
+			Bool()
 )
 
 func main() {
@@ -54,6 +62,19 @@ func main() {
 		if len(subset) == 0 {
 			log.Fatalf("Unable to find an element with the class '%s'\n", *byClass)
 		}
+	} else if *byAttribute != "" {
+		keyVal := strings.SplitN(*byAttribute, "=", 2)
+		key := keyVal[0]
+		var value string
+		if len(keyVal) == 1 {
+			value = ""
+		} else {
+			value = keyVal[1]
+		}
+		subset = scrape.FindAll(root, matchByAttribute(key, value))
+		if len(subset) == 0 {
+			log.Fatalf("Unable to find an element with attribute matcher %s", *byAttribute)
+		}
 	} else {
 		subset = append(subset, root)
 	}
@@ -62,4 +83,17 @@ func main() {
 	}
 
 	fmt.Print(string(asJson))
+}
+
+func matchByAttribute(key, value string) scrape.Matcher {
+	return func(node *html.Node) bool {
+		if node.Type == html.ElementNode {
+			result := scrape.Attr(node, key)
+			//fmt.Printf("Saw %s and result %s with attrs %+v\n", node.Data, result, node.Attr)
+			if result != "" && (value == "" || value == result) {
+				return true
+			}
+		}
+		return false
+	}
 }
